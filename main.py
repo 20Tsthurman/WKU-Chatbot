af52fef5ebcd2ddba2f5e5d5e85ae083feb9695d
Tanner <tsthurman03@gmail.com>
2024-03-05 11:30:47 -0600

    3/5/24 Update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from models.train import train_model
 
 def main():
     data_file = 'data/training_data.csv'
-    input_length = 20  # Assuming a fixed input length for simplicity
+    input_length = 4  # Assuming a fixed input length for simplicity
     
     # Train the chatbot model
     train_model(data_file, input_length)
